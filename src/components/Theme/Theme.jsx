edb22159287c93b839c779c684ef8765a30b6840
Avyukt <avyuktrkumar08@gmail.com>
2024-01-23 13:24:18 +0530

    theme page updated

--- src/components/Theme/Theme.jsx
+++ src/components/Theme/Theme.jsx
@@ -7,19 +7,7 @@ const Theme = () => {
         <div><Fade bottom><h1 className="theme_title"> OUR THEME</h1></Fade></div>
       <div className="content_box">
       <p className="theme_content">
-      "God creates dinosaurs, God destroys dinosaurs. God creates Man, Man destroys God. Man creates dinosaurs."
-
-This iconic dialogue from Jurassic Park provided the perfect theme for Team SANSKRITI's ECLECTIKA'23. 
-
-When you're surrounded by the undisputed rulers of the planet, life should find a way to survive! 
-
-Are you excited??? 
-Ooohh, Aah,_
-That's how it always starts. But then later, there's running and screaming.
-
-We just want to say Hold on to your butts in Jurassic World jargon.
-
-Get ready for Time Travel into the prehistoric era! Fasten your seatbelts. 
+      Get ready for an exhilarating experience at our superhero-themed event, where ordinary moments transform into extraordinary memories. Unleash your inner hero as we bring the vibrant world of comic books and blockbuster films to life. From thrilling cosplay showcases to interactive superhero challenges, immerse yourself in a day filled with action-packed fun for all ages. Join forces with fellow fans, meet iconic characters, and celebrate the spirit of heroism in a dynamic atmosphere that's perfect for families, friends, and superhero enthusiasts alike. Embrace the adventure, strike your best superhero pose, and make lasting memories at our unforgettable superhero-themed event!
           </p>
       </div>
     </div>
